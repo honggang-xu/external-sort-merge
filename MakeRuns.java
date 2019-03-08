44a1be34b1e1017fb016672c4c380f4a8ff0bcc3
honggang-xu <41971841+honggang-xu@users.noreply.github.com>
2019-03-09 11:37:52 +1300

    Add files via upload

---
+++ MakeRuns.java
@@ -0,0 +1,15 @@
+public class MakeRuns
+{
+	private static int size;
+
+	//accept a single integer argument specifying the size of the MinHeap
+	public static void main(String [] args)
+	{
+
+	}
+
+	public void printRuns()
+	{
+
+	}
+}
---
+++ MinHeap.java
@@ -0,0 +1,19 @@
+public class MinHeap
+{
+	private List<String> data;
+	
+	public void insert()
+	{
+
+	}
+
+	public void remove()
+	{
+
+	}
+
+	public void replace()
+	{
+
+	}
+}
