0798308d92e8aa8639d86255b4ada3b38618cfad
honggang-xu <41971841+honggang-xu@users.noreply.github.com>
2019-03-13 21:38:36 +1300

    MakeRuns is ready for use

--- MakeRuns.java
+++ MakeRuns.java
@@ -1,5 +1,3 @@
-//the size of each run may differ. only when the heap capacity is 0,]
-//the run is finished
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileWriter;
@@ -11,31 +9,30 @@ public class MakeRuns
 	private static int runs;
 	private static MinHeap heap;
 
-	//accept a single integer argument specifying the size of the MinHeap
-	//and a single file name as the input file
+	/*
+	accept a single integer argument specifying the size of the MinHeap
+	and a single file name as the input file
+	*/
 	public static void main(String [] args)
 	{
 		try
 		{
 			size = Integer.parseInt(args[0]);
 			heap = new MinHeap(size);
-			System.out.println("created heap");
 
 			String inputFileName = args[1];
 			BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
-			System.out.println("input file identified");
 
 			FileWriter file = new FileWriter("runs.txt");
 			PrintWriter writer = new PrintWriter(file);
-			System.out.println("output file created");
-			
 
 			replacementSelection(reader, writer);
-			System.out.println("Number of Runs:" + runs);
+			System.out.println("Process Finished");
+			System.out.println("Number of Runs: " + runs);
 		}
 		catch (Exception ex)
 		{
-			System.err.println("invalid input");
+			System.err.println("usage: <number> <file>");
 			ex.printStackTrace();
 		}		
 	}
@@ -52,22 +49,12 @@ public class MakeRuns
 				if ((line = inputFile.readLine()) != null)
 					heap.insert(line);
 			}
-			System.out.println("finished fill up the heap");
-
-			for (int i = 0; i < size; i++)
-				System.out.println(heap.get(i));
-			System.out.println("printed the heap");
 
 			String lastOutput = null;
 			boolean hasInput = true;
 			while (hasInput)
-			{	//System.out.println(line);
-				//System.out.println("check if root can go into run");
-				//System.out.println(heap.peek());
-				//System.out.println("displayed the root");
+			{	
 				//while root can go into current run
-
-
 				if (lastOutput == null || !heap.smallerThan(heap.peek(), lastOutput))
 				{
 					if ((line = inputFile.readLine()) != null)
@@ -75,8 +62,6 @@ public class MakeRuns
 						//use replace method to put the String in heap
 						lastOutput = heap.replace(line);
 						writer.println(lastOutput);
-						System.out.println(lastOutput);
-						System.out.println("output one String");
 					}
 					else
 					{
@@ -84,58 +69,39 @@ public class MakeRuns
 					}
 					
 				}
+				//if root cannot go into the current run
 				else if (heap.smallerThan(heap.peek(), lastOutput))
 				{
-					System.out.println("root cannot go into run");
 					heap.remove();
-					System.out.println("removed root from heap");
 				}
 
-				System.out.println("check if heap's capacity is reduced to 0");
+				//check if heap's capacity is reduced to 0
 				if (heap.capacity() == 0)
 				{
-					writer.println("end of run");
+					writer.println("-end of run");
 					runs++;
 					heap.reheap(size);
-					System.out.println("capacity hits 0 and reheap");
 					heap.setCapacity(size);
 					lastOutput = null;
 				}
-				System.out.println("finished reading one line");
-			
 			}
-		}
-		
-		catch (Exception ex)
-		{
-			ex.printStackTrace();
-		}
-		
-		System.out.println("the capacity of the heap is:");
-		System.out.println(heap.capacity());
-		//when inputFile is empty & heap is not empty
-		
-		//if (heap.capacity() != 0)
-		//{
-			System.out.println("entered if statement");
-
-
-			//this reheap don't finish
+			//when inputFile is empty, reheap the heap and output the heap
 			heap.reheap(size);
-			System.out.println("finished reheap");
 
 			for (int i = 0; i < size; i++)
 			{
-				System.out.println("entered for loop");
 				writer.println(heap.get(i));
-				System.out.println(heap.get(i));
 			}
-			writer.println("end of run");
+			writer.println("-end of run");
 			runs++;
-			writer.println("end of file");
-			System.out.println("all the runs have ended");
-		//}
-		writer.close();
+			writer.println("-end of file");
+			writer.println("-number of runs:");
+			writer.println(runs);
+			writer.close();
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}	
 	}
-
 }
