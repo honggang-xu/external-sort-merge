e354b26b063c0a3696677f4e4053b0731ed7e699
honggang-xu <41971841+honggang-xu@users.noreply.github.com>
2019-03-22 14:15:42 +1300

    Bug fixes for MinHeap and MakeRuns

--- MakeRuns.java
+++ MakeRuns.java
@@ -39,19 +39,26 @@ public class MakeRuns
 
 	private static void replacementSelection(BufferedReader inputFile, PrintWriter writer)
 	{
-		String line;
+		String line = null;
+		int removed = 0;
 		try 
 		{
+			String lastOutput = null;
+			boolean hasInput = true;
 			//fill up the heap
 			while (heap.capacity() < size)
 			{
 				//read a string
 				if ((line = inputFile.readLine()) != null)
 					heap.insert(line);
+				else
+				{
+					hasInput = false;
+					break;
+				}
 			}
 
-			String lastOutput = null;
-			boolean hasInput = true;
+			
 			while (hasInput)
 			{	
 				//while root can go into current run
@@ -72,28 +79,55 @@ public class MakeRuns
 				//if root cannot go into the current run
 				else if (heap.smallerThan(heap.peek(), lastOutput))
 				{
+					if (line == null)
+						hasInput= false;
 					heap.remove();
+					removed++;
 				}
 
 				//check if heap's capacity is reduced to 0
 				if (heap.capacity() == 0)
 				{
 					writer.println("-end of run");
+					//////
+					//writer.println("when heap is empty");
 					runs++;
 					heap.reheap(size);
 					heap.setCapacity(size);
 					lastOutput = null;
+					removed = 0;
 				}
 			}
 			//when inputFile is empty, reheap the heap and output the heap
-			heap.reheap(size);
-
-			while (heap.capacity() != 0)
+			heap.setCapacity(heap.capacity() + removed);
+			//writer.println("removed: " + removed);
+			//writer.println("capacity: " + heap.capacity());
+			System.out.println("about to enter reheap process");
+			System.out.println("capacity is: " + heap.capacity());
+			heap.reheap(heap.capacity());
+			System.out.println("finished reheap");
+			if (heap.capacity() > 0)
 			{
-				writer.println(heap.remove());
+				if (removed != 0)
+				{
+					writer.println("-end of run");
+					runs++;
+					///
+					//writer.println("with no more input");
+				}
+				
+				while (heap.capacity() != 0)
+				{
+					String out = heap.remove();
+					if (out != null)
+						writer.println(out);
+				}
+				writer.print("-end of run");
+				///
+				//writer.println("the final indication");
+				runs++;
 			}
-			writer.print("-end of run");
-			runs++;
+			
 			//writer.print("-end of file");
 			//writer.println("-number of runs:");
 			//writer.println(runs);
